e93ae22831cd1396c2d28f85d2dc52bdbd1d6ed7
gsalaman <43499190+gsalaman@users.noreply.github.com>
2019-01-15 08:30:22 -0700

    examples

--- pointer_examples.ino
+++ pointer_examples.ino
@@ -1,46 +1,74 @@
 #include "SoftwareSerial.h"
 #include "memprint.h"
 
-int myInt;
-int *intPtr;
-int secondInt;
+int myArray[5]={10,11,12,13,14};
+
+#define ARR_SIZE(arr) (sizeof(arr)/sizeof(arr[0]))
+
+int stringLength(char *str)
+{
+  int count=0;
+
+  while (*str)
+  {
+    count++;
+    str++;
+  }
+
+  return count;
+}
+
+void string_length_excercise( void )
+{
+  char myString[] = "Sriracha";
+  int  myStringLength;
+
+  myStringLength = stringLength(myString);
   
+  Serial.print("String length = ");
+  Serial.println(stringLength(myString));
+  
+}
+
 void setup( void )
 {
   Serial.begin(9600);
 
-  Serial.println("Addresses:");
-  Serial.print("myInt = 0x");
-  Serial.println((unsigned long) &myInt, HEX);
-  Serial.print("intPtr = 0x");
-  Serial.println((unsigned long) &intPtr, HEX);
-  Serial.print("secondInt = 0x");
-  Serial.println((unsigned long) &secondInt, HEX);
-  Serial.println("============");
+  int *intPtr;
+  int myInt;
+  int i;
+
+  intPtr = myArray;
+  myInt = *intPtr;
   
-  myInt = 4;
-  
+  Serial.print("myArray = ");
+  Serial.println((unsigned long) myArray, HEX);
+
   Serial.print("myInt = ");
   Serial.println(myInt);
-  
-  intPtr = &myInt;
-  
-  Serial.print("intPtr = ");
-  Serial.println((unsigned long) intPtr, HEX);
-  Serial.print("Address of intPtr = ");
-  Serial.println((unsigned long) &intPtr, HEX);
-  
-  secondInt = *intPtr;
-  *intPtr = 7;
-  
-  Serial.print("myInt = ");
-  Serial.println(myInt);
-  Serial.print("secondInt = ");
-  Serial.println(secondInt);
 
-  Serial.println("==========");
+  Serial.print("myArray size (bytes): ");
+  Serial.println(sizeof(myArray));
+  Serial.print("intPtr size: ");
+  Serial.println(sizeof(intPtr));
 
-  memprint_bytes(&secondInt);
+  Serial.print("myArray # elements: ");
+  Serial.println(ARR_SIZE(myArray));
+
+  for (i=0; i<ARR_SIZE(myArray); i++)
+  {
+    Serial.print("intPtr = ");
+    Serial.print((unsigned long) intPtr, HEX);
+    Serial.print(" *intPtr = ");
+    Serial.println(*intPtr);
+
+    intPtr++;
+  }
+  
+  
+  memprint_bytes(myArray);
+
+  string_length_excercise();
 }
 
 void loop ( void )
